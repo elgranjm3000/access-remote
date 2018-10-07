106986185832958c0f3b1798316ceebbd08210de
Joseph Muentes <elgranjm3000@gmail.com>
2018-10-07 13:02:22 -0400

    CLIENTES

--- public/assets/js/demo/demo.js
+++ public/assets/js/demo/demo.js
@@ -15,8 +15,8 @@ var Demo = function() {
         return;
       }
       e.preventDefault;
-      alert('Your form has submitted!');
-      return false;
+      //alert('Your form has submitted!');
+      //return false;
     });
 
     // give file-upload preview onclick functionality
--- src/Form/ClientesType.php
+++ src/Form/ClientesType.php
@@ -3,9 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Clientes;
+use App\Entity\User;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class ClientesType extends AbstractType
 {
@@ -16,15 +22,38 @@ class ClientesType extends AbstractType
             ->add('nit')
             ->add('contactoPrincipal')
             ->add('correo')
-            ->add('fechaNacimiento')
-            ->add('comentarios')
+            ->add('fechaNacimiento', DateType::class, array(
+    'widget' => 'single_text',
+
+    // prevents rendering it as type="date", to avoid HTML5 date pickers
+    'html5' => false,
+
+    // adds a class that can be selected in JavaScript
+    'attr' => ['class' => 'js-datepicker'],
+))
+            ->add('comentarios', CKEditorType::class, array(
+                        'config' => array('toolbar' => 'basic'),
+                ))
             ->add('zona')
             ->add('dpto')
             ->add('municipio')
             ->add('telefonoMovil')
             ->add('telefonoLocal')
-            ->add('direccion')
-            ->add('IdUsuario')
+            ->add('direccion', CKEditorType::class, array(
+                        'config' => array('toolbar' => 'basic'),
+                ))
+            ->add('IdUsuario', EntityType::class, array(
+    // looks for choices from this entity
+    'class' => User::class,
+
+    // uses the User.username property as the visible option string
+    'choice_label' => 'username',
+     'placeholder' => 'Seleccione un usuario'
+
+    // used to render a select box, check boxes or radios
+    // 'multiple' => true,
+    // 'expanded' => true,
+))
         ;
     }
 
